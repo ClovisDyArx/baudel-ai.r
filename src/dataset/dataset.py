a4f1ad3f2592a8778088619e6929d170199e98ca
Clovis Lechien <clovis.lechien@epita.fr>
2024-11-21 16:26:32 +0100

    feat(dataset): better preprocessing

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,4 +1,34 @@
 import pandas as pd
+import re
+
+
+def normalize_text(text):
+    text = re.sub(r'\r\n|\r|\n', ' ', text)
+    text = re.sub(r'\t', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    return text
+
+
+def add_special_tokens(text):
+    return "<|startoftext|>" + text + "<|endoftext|>"
+
+
+def preprocess_df(df_poetry, df_love_poems):
+    # normalize the content of the poems
+    df_poetry['content'] = df_poetry['content'].apply(normalize_text)
+    df_love_poems['poem'] = df_love_poems['poem'].apply(normalize_text)
+
+    # concat the two dataframes
+    df = pd.concat([df_poetry['content'], df_love_poems['poem']], ignore_index=True)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # add special tokens - GPT2
+    df = df.apply(add_special_tokens)
+
+    return df
 
 
 class DatasetPoem:
@@ -6,7 +36,7 @@ class DatasetPoem:
         df_poetry = pd.read_csv(poetry_path)
         df_love_poems = pd.read_parquet(love_poems_path)
 
-        self.df = self.preprocess_df(df_poetry, df_love_poems)
+        self.df = preprocess_df(df_poetry, df_love_poems)
 
     def display_dataset(self):
         print("Poems Dataset : \n")
@@ -15,17 +45,11 @@ class DatasetPoem:
     def get_df(self):
         return self.df
 
-    def preprocess_df(self, df_poetry, df_love_poems):
-        df = df_poetry['content']
-        # concat the two dataframes
-        df = pd.concat([df, df_love_poems['poem']])
-        # drop duplicates
-        df.drop_duplicates(inplace=True)
-        return df
 
+if __name__ == "__main__":
+    poetry_path = "hf://datasets/merve/poetry/poetry.csv"
+    love_poems_path = "hf://datasets/asoria/love-poems/data/train-00000-of-00001.parquet"
+    dataset = DatasetPoem(poetry_path, love_poems_path)
 
-poetry_path = "hf://datasets/merve/poetry/poetry.csv"
-love_poems_path = "hf://datasets/asoria/love-poems/data/train-00000-of-00001.parquet"
-dataset = DatasetPoem(poetry_path, love_poems_path)
-
-dataset.display_dataset()
+    df = dataset.get_df()
+    print(df)
