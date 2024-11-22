0f51ea3a9b4cc9980ea34b96a5f4e4ab84202f94
Clovis Lechien <clovis.lechien@epita.fr>
2024-11-22 21:18:40 +0100

    feat(training): I dont have enough gpu memory to fine tune gpt2 large, I will be working on gpt2 medium for the rest of the project.

--- src/llm/inference.py
+++ src/llm/inference.py
@@ -1,7 +1,7 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_path = "./results/checkpoint-1518"
-model_name = "gpt2-medium"
+model_path = "./finetuned_models/gpt2-medium-500poems"
+model_name = "gpt2-medium"  # TODO: "gpt2-medium", "gpt2-large" or "gpt2-xl"
 
 model = GPT2LMHeadModel.from_pretrained(model_path)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
@@ -16,7 +16,7 @@ output = model.generate(
     min_length=50,             # Minimum number of tokens in the generated text
     max_length=100,            # Maximum number of tokens in the generated text
     num_return_sequences=1,    # Number of sequences to generate
-    temperature=0.7,           # Sampling temperature (higher = more random)
+    temperature=0.5,           # Sampling temperature (higher = more random)
     top_k=50,                  # Consider top-k tokens for sampling
     top_p=0.95,                # Nucleus sampling (top-p tokens' cumulative probability >= 0.95)
     do_sample=True             # Enable sampling
--- src/llm/training.py
+++ src/llm/training.py
@@ -4,23 +4,17 @@ import wandb
 from dotenv import load_dotenv
 import os
 
-# Load environment variables for WandB
 load_dotenv(dotenv_path='env/wandb.env')
 wandb.login(key=os.getenv('WANDB_API_KEY'))
 
-# Load GPT-2 model and tokenizer
-model_name = "gpt2-medium"  # TODO: try "gpt2-large" or "gpt2-xl"
+model_name = "gpt2-medium"  # TODO: "gpt2-medium", "gpt2-large" or "gpt2-xl"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# Load and preprocess dataset
 dataset = DatasetPoem()
 tokenized_data = dataset.tokenize(tokenizer)
-
-# Convert tokenized data into a PyTorch Dataset
 poems_dataset = PoemsDataset(tokenized_data)
 
-# Define training arguments
 training_args = TrainingArguments(
     output_dir="./results",            # Directory to save model checkpoints
     num_train_epochs=3,                # Number of epochs
@@ -28,21 +22,19 @@ training_args = TrainingArguments(
     per_device_eval_batch_size=1,      # Evaluation batch size
     logging_dir="./logs",              # Directory for logs
     logging_steps=10,                  # Log every 10 steps
-    save_steps=500,                    # Save checkpoint every 500 steps
-    eval_strategy="steps",       # Evaluate at every step
+    save_steps=1000,                   # Save checkpoint every 500 steps
+    eval_strategy="steps",             # Evaluate at every step
     report_to="wandb",                 # Use WandB for logging
     learning_rate=5e-5,                # Learning rate
     warmup_steps=50,                   # Warmup steps
     weight_decay=0.01,                 # Weight decay
 )
 
-# Initialize Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=poems_dataset,  # Pass the custom dataset
-    eval_dataset=poems_dataset,   # Use the same dataset for evaluation (for simplicity)
+    train_dataset=poems_dataset,
+    eval_dataset=poems_dataset,
 )
 
-# Start training
 trainer.train()
