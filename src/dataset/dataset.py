14eb6eeab2aa19f3b7d45196ada1fcc710bce80a
Clovis Lechien <clovis.lechien@epita.fr>
2024-11-22 21:05:22 +0100

    feat(dataset): 20k poems now, will try to finetune gpt2 large on the new dataset.

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,6 +1,33 @@
 from torch.utils.data import Dataset
 import pandas as pd
 import re
+import kagglehub
+import os
+
+# dataset paths
+poetry_path = "hf://datasets/merve/poetry/poetry.csv"
+love_poems_path = "hf://datasets/asoria/love-poems/data/train-00000-of-00001.parquet"
+poem_classification_path = "ramjasmaurya/poem-classification-nlp"
+poetryfoundationorg_path = "johnhallman/complete-poetryfoundationorg-dataset"
+poetry_foundation_path = "tgdivy/poetry-foundation-poems"
+
+
+def list_files(directory):
+    files = []
+    for root, dirs, file_names in os.walk(directory):
+        for file_name in file_names:
+            files.append(os.path.join(root, file_name))
+    return files
+
+
+def load_kaggle_datasets(dataset_path):
+    path = kagglehub.dataset_download(dataset_path)
+    files = list_files(path)
+    datasets = [pd.read_csv(file) for file in files]
+    dataset = pd.concat(datasets, ignore_index=True).copy()
+    dataset.dropna(inplace=True)
+    dataset.drop_duplicates(inplace=True)
+    return dataset
 
 
 def normalize_text(text):
@@ -15,13 +42,18 @@ def add_special_tokens(text):
     return "<|startoftext|>" + text + "<|endoftext|>"
 
 
-def preprocess_df(df_poetry, df_love_poems):
+def preprocess_df(df_poetry, df_love_poems, df_classification, df_poetryfoundationorg, df_poetry_foundation):
+
     # normalize the content of the poems
     df_poetry['content'] = df_poetry['content'].apply(normalize_text)
     df_love_poems['poem'] = df_love_poems['poem'].apply(normalize_text)
+    df_classification['Poem'] = df_classification['Poem'].apply(normalize_text)
+    df_poetryfoundationorg['Content'] = df_poetryfoundationorg['Content'].apply(normalize_text)
+    df_poetry_foundation['Poem'] = df_poetry_foundation['Poem'].apply(normalize_text)
+    objs = [df_poetry['content'], df_love_poems['poem'], df_classification['Poem'], df_poetryfoundationorg['Content'], df_poetry_foundation['Poem']]
 
     # concat the two dataframes
-    df = pd.concat([df_poetry['content'], df_love_poems['poem']], ignore_index=True).to_frame(name='poem')
+    df = pd.concat(objs, ignore_index=True).to_frame(name='poem')
 
     # drop duplicates
     df.drop_duplicates(inplace=True)
@@ -34,11 +66,14 @@ def preprocess_df(df_poetry, df_love_poems):
 
 # Dataset class : Raw data -> Dataframe
 class DatasetPoem:
-    def __init__(self, poetry_path="hf://datasets/merve/poetry/poetry.csv", love_poems_path="hf://datasets/asoria/love-poems/data/train-00000-of-00001.parquet"):
+    def __init__(self):
         df_poetry = pd.read_csv(poetry_path)
         df_love_poems = pd.read_parquet(love_poems_path)
+        df_classification = load_kaggle_datasets(poem_classification_path)
+        df_poetryfoundationorg = load_kaggle_datasets(poetryfoundationorg_path)
+        df_poetry_foundation = load_kaggle_datasets(poetry_foundation_path)
 
-        self.df = preprocess_df(df_poetry, df_love_poems)
+        self.df = preprocess_df(df_poetry, df_love_poems, df_classification, df_poetryfoundationorg, df_poetry_foundation)
 
     def display_dataset(self):
         print("Poems Dataset : \n")
@@ -70,5 +105,5 @@ class PoemsDataset(Dataset):
         return {
             "input_ids": self.input_ids[idx],
             "attention_mask": self.attention_mask[idx],
-            "labels": self.input_ids[idx],  # For language modeling, labels are the same as input_ids
+            "labels": self.input_ids[idx],
         }
